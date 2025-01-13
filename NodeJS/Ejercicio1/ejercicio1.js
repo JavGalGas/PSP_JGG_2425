89ca1706ded458b5e37b8c7959e38cee917bd432
JavGalGas <145196986+JavGalGas@users.noreply.github.com>
2025-01-13 12:29:41 +0100

    commit 14

---
+++ NodeJS/Ejercicio1/ejercicio1.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const myModule = require('./modules/my_module.js');
+
+const server = http.createServer((request, response) => {
+    let filePath = '.' + request.url;
+    if (filePath === './') {
+        filePath = './index.html';
+    }
+
+    const extname = String(path.extname(filePath)).toLowerCase();
+    const contentType = myModule.getContentType(extname);
+
+    fs.readFile(filePath, (error, content) => {
+        if (error) {
+            if (error.code === 'ENOENT') {
+                response.writeHead(404, { 'Content-Type': 'text/html' });
+                response.end('<h1>404 Not Found</h1>', 'utf-8');
+            } else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
+            }
+        } else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+});
+
+server.listen(80, () => {
+    console.log('Server running at http://127.0.0.1:80/');
+});
---
+++ NodeJS/Ejercicio1/modules/my_module.js
@@ -0,0 +1,13 @@
+function getContentType(ext) {
+    switch (ext) {
+        case '.html':
+            return 'text/html';
+        case '.css':
+            return 'text/css';
+        case '.jpg':
+            return 'image/jpeg';
+        default:
+            return 'application/octet-stream';
+    }
+}
+module.exports = getContentType;
