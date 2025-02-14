af4028ec83b3d784920ebd96f2b2cf4a59b28e2f
JavGalGas <145196986+JavGalGas@users.noreply.github.com>
2025-02-14 11:22:05 +0100

    commit 17

--- NodeJS/Ejercicio1/ejercicio1.js
+++ NodeJS/Ejercicio1/ejercicio1.js
@@ -1,23 +1,53 @@
 const http = require('http');
-const fs = require('fs');
+const { URL } = require('url');
 const path = require('path');
+const fs = require('fs');
 const myModule = require('./modules/my_module.js');
 
 const server = http.createServer((request, response) => {
-    let filePath = '.' + request.url;
-    if (filePath === './') {
-        filePath = './index.html';
+    const url_parts = new URL(request.url, `http://${request.headers.host}`);
+    console.log(url_parts);
+
+    let filePath = '.' + url_parts.pathname;
+    console.log(filePath);
+    if (filePath === './index') {
+        console.log('entra en index');
+        switch (url_parts.searchParams.get('lang') || 'es') {
+            case 'en':
+                filePath = './enindex.html';
+                break;
+            default:
+                filePath = './index.html';
+        }
     } else if (filePath === './pagina1') {
-        filePath = './pagina1.html';
+        console.log('entra en pagina 1');
+        switch (url_parts.searchParams.get('lang') || 'es') {
+            case 'en':
+                filePath = './enpagina1.html';
+                break;
+            default:
+                filePath = './pagina1.html';
+        }
     } else if (filePath === './pagina2') {
-        filePath = './pagina2.html'
+        console.log('entra en pagina 2');
+        switch (url_parts.searchParams.get('lang') || 'es') {
+            case 'en':
+                filePath = './enpagina2.html';
+                break;
+            default:
+                filePath = './pagina2.html';
+        }
     }
+    console.log(filePath);
 
     const extname = String(path.extname(filePath)).toLowerCase();
+    console.log(extname);
     const contentType = myModule.getContent(extname);
+    console.log(contentType);
 
     fs.readFile(filePath, (error, content) => {
         if (error) {
+            console.log(error);
             if (error.code === 'ENOENT') {
                 response.writeHead(404, { 'Content-Type': 'text/html' });
                 response.end('<h1>404 Not Found</h1>', 'utf-8');
@@ -26,12 +56,14 @@ const server = http.createServer((request, response) => {
                 response.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
             }
         } else {
+            const color = url_parts.searchParams.get('color') || 'white';
+            const modifiedContent = content.replace('</head>', `<style>body { background-color: ${color}; }</style></head>`);
             response.writeHead(200, { 'Content-Type': contentType });
-            response.end(content, 'utf-8');
+            response.end(modifiedContent, 'utf-8');
         }
     });
 });
 
 server.listen(80, () => {
-    console.log('Server running at: localhost');
+    console.log(`Server running at: ${server.address().port}`);
 });
